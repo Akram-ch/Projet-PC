178700df4f78275369b16516fd9e8b41d932cfc1
Akram Chaouache <akramchaouache@gmail.com>
2022-12-05 10:55:53 +0100

    obj 3

--- Prodcons/src/prodcons/v3/ProdConsBuffer.java
+++ Prodcons/src/prodcons/v3/ProdConsBuffer.java
@@ -6,60 +6,62 @@ import prodcons.IProdConsBuffer;
 import prodcons.Message;
 
 public class ProdConsBuffer implements IProdConsBuffer {
-	private Semaphore notFull;
-	private Semaphore notEmpty;
-	private Semaphore mutex;
-	private Message[] messages;
+
+	Message[] messages;
 	private int buffSize;
-	private int nfull;
-	private int nempty;
 	private int in;
 	private int out;
 	private int totmsg;
+	private Semaphore notFull;
+	private Semaphore notEmpty;
+	private Semaphore mutexIn;
+	private Semaphore mutexOut;
 
-	public ProdConsBuffer(int buffSz) {
-		messages = new Message[buffSz];
-		notFull = new Semaphore(buffSz);
-		notEmpty = new Semaphore(0);
-		mutex = new Semaphore(1);
-		buffSize = buffSz;
-		nfull = 0;
-		nempty = buffSize;
+	ProdConsBuffer(int bsize) {
+		buffSize = bsize;
+		messages = new Message[buffSize];
 		in = 0;
 		out = 0;
 		totmsg = 0;
-
+		notFull = new Semaphore(buffSize);
+		notEmpty = new Semaphore(0);
+		mutexIn = new Semaphore(1);
+		mutexOut = new Semaphore(1);
 	}
 
-	@Override
 	public void put(Message m) throws InterruptedException {
-		// TODO Auto-generated method stub
-		notFull.acquire();
-		mutex.acquire();
-		messages[in] = m;
-		in = (in + 1) % buffSize;
-		totmsg++;
-		mutex.release();
-		notEmpty.release();
-
+		try {
+			notFull.acquire();
+			mutexIn.acquire();
+			messages[in] = m;
+			in = (in + 1) % buffSize;
+			totmsg++;
+			Consumer.nbLectures++;
+		} finally {
+			mutexIn.release();
+			notEmpty.release();
+		}
 	}
 
 	@Override
 	public Message get() throws InterruptedException {
-		// TODO Auto-generated method stub
-		notEmpty.acquire();
-		mutex.acquire();
-		Message result = messages[out];
-		out = (out + 1) % buffSize;
-		mutex.release();
-		notFull.release();
+		Message result = new Message();
+		try {
+			notEmpty.acquire();
+			mutexOut.acquire();
+			result = messages[out];
+			Consumer.nbLectures--;
+			out = (out + 1) % buffSize;
+		} finally {
+			mutexOut.release();
+			notFull.release();
+		}
 		return result;
 	}
 
 	@Override
 	public int nmsg() {
-		// TODO Auto-generated method stub
-		return notFull.availablePermits();
+		return notEmpty.availablePermits();
 	}
 
 	@Override
